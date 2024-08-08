a1bc65ae6a3450025e3da5502b16635fb483525a
Akın Kocabıyık <70774823+AknKocabyk@users.noreply.github.com>
2024-08-09 01:42:17 +0300

    Color Change

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Objenin hareket hızı.
+    public float moveSpeed = 5f;
+
+    // Hareket sınırlarını belirlemek için minimum ve maksimum değerler.
+    public Vector2 minBounds; // Sol alt köşe
+    public Vector2 maxBounds; // Sağ üst köşe
+
+    void FixedUpdate()
+    {
+        // Sağ-sol hareketi için yatay eksen girdisini alıyoruz.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        // Yukarı-aşağı hareketi için dikey eksen girdisini alıyoruz.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Yeni pozisyonu hesaplıyoruz.
+        Vector3 newPosition = transform.position + new Vector3(moveHorizontal * moveSpeed * Time.deltaTime, moveVertical * moveSpeed * Time.deltaTime, 0f);
+
+        // Objenin pozisyonunu sınırlar içinde tutuyoruz.
+        newPosition.x = Mathf.Clamp(newPosition.x, minBounds.x, maxBounds.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, minBounds.y, maxBounds.y);
+
+        // Objenin pozisyonunu güncelliyoruz.
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour
+{
+    private SpriteRenderer playerSpriteRenderer;
+
+    private Color[] colors = { Color.red, Color.blue, Color.green, Color.yellow };
+
+    // Şu anki renk indeksini tutar.
+    private int currentColorIndex = 0;
+    void Start()
+    {
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+
+        playerSpriteRenderer.color = colors[currentColorIndex];
+    }
+
+    void Update()
+    {
+        // Q tuşuna basıldığında renk değiştiriyoruz.
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // Bir sonraki renge geçiyoruz.
+            currentColorIndex = (currentColorIndex + 1) % colors.Length;
+
+            // Objenin rengini güncelliyoruz.
+            playerSpriteRenderer.color = colors[currentColorIndex];
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SpriteRenderer otherSpriteRenderer = collision.gameObject.GetComponent<SpriteRenderer>();
+
+        // Eğer çarpışılan obje bir SpriteRenderer içeriyorsa ve rengi objemizin rengiyle aynıysa
+        if (otherSpriteRenderer != null && playerSpriteRenderer.color == otherSpriteRenderer.color)
+        {
+            // Objeyi engelden geçiriyoruz.
+            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+        }
+    }
+}
